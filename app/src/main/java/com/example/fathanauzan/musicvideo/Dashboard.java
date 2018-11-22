e10a05d758e6c7288e7a01060abb96a4b8fedd79
jassom15 <abdullahfathan@gmail.com>
2018-11-22 08:37:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fathanauzan/musicvideo/Dashboard.java
@@ -0,0 +1,20 @@
+package com.example.fathanauzan.musicvideo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fathanauzan.musicvideo.Dashboard;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
+
